6f200b862a19a508b0f3a8720f5a3cddc42a3586
rodrigoBojorquez <rbojorquez1620@gmail.com>
2024-02-08 22:28:03 -0500

    endpoints for add and get doctors work experience are finished, working on update and delete endpoints

--- src/database.py
+++ src/database.py
@@ -12,3 +12,12 @@ if "email_1" not in db.users.index_information():
 
 if "license_number_1" not in db.users.index_information():
     db["users"].create_index("license_number", unique=True)
+
+# db["users"].create_index(
+#     ["email", 
+#      "experience.clinic", 
+#      "experience.work", 
+#      "experience.from_date", 
+#      "experience.to_date"],
+#      unique=True
+# )
--- src/routers/doctors.py
+++ src/routers/doctors.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from typing_extensions import Annotated
 from ..database import db
-from ..data_models import Degree, ObjectId
+from ..data_models import Degree, ObjectId, Experience
+from bson import ObjectId as _ObjectId
 from icecream import ic
 
 # OAUTH SCHEME
@@ -16,17 +17,64 @@ router = APIRouter(
 @router.get('/')
 async def get_all_doctors():
     collecion = db["users"]
-    doctors = collecion
+    response = collecion.find()
+    doctors = []
+    for doctor in response:
+        doctor["_id"] = str(doctor["_id"])
+        doctors.append(doctor)
+    ic(doctors)
     return JSONResponse(content={"message": "successfull request", "token": doctors}, status_code=201)
 
 @router.post("/education/{id}")
 async def add_degree(id: ObjectId, degree: Degree):
     collection = db["users"]
     doctor = collection.find_one({"_id": id,  "role": "doctor"})
-    ic(doctor)
     if not doctor:
         raise HTTPException(status_code=404, detail="doctor with this id does not exist")
     return JSONResponse(
         content={"message": "degree added successfully",  "degree": degree},
         status_code=201
     )
+
+@router.post("/experience/{id}")
+async def add_doctor_experience(id: ObjectId, experience: Experience):
+    doctor = await validate_doctor(id)
+    await validate_experience(doctor, experience)
+    response = db["users"].update_one(
+        {"_id": _ObjectId(id)},
+        { "$push": {"experience": experience.model_dump()}}
+    )
+    return JSONResponse(content={"message": "work experience aggregate successfully"}, status_code=201)
+
+@router.get("/experience/{id}")
+async def get_doctor_experience(id: ObjectId):
+    doctor = await validate_doctor(id)
+    exp = doctor["experience"]
+    return  JSONResponse(content={
+        "message":"successfull request",
+        "data":exp
+    },status_code=200)
+
+'''
+    TODO:
+        - agregar validacion de maximo 10 experiencias laborales
+        - agregar un ID a cada experiencia laboral
+'''
+
+@router.put("/experience/{id}")
+async def update_doctor_experience(id: ObjectId, newExperience: Experience):
+    return
+
+async def validate_doctor(id: _ObjectId):
+    ic(id)
+    collection = db["users"]
+    doctor = collection.find_one({"_id": _ObjectId(id), "rol": "doctor"})
+    if not doctor:
+        raise HTTPException(status_code=404, detail="doctor not found")
+    return doctor
+
+async def validate_experience(doctor, experience):
+    existing_exp = next((exp for exp in doctor["experience"] if exp == experience.model_dump()), None)
+    if existing_exp:
+        raise HTTPException(status_code=400, detail="this work experience is already registered")
+    return
